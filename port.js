d313aa45b7388592b45306718dba62ac8430bdd9
abhishek <abhishektupsundar1@gmail.com>
2023-11-23 14:09:53 +0530

    Done till about

---
+++ port.js
@@ -0,0 +1,7 @@
+var Typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Web Developer", "MERN STACK DEVELOPER"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
